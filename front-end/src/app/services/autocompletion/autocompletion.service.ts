edb0b09c3dbaa16d2bdda9c858f4a6cfb70d6223
Renel Lherisson <renellherisson@gmail.com>
2025-01-13 11:29:31 -0500

    RL | completed auto completion service

--- front-end/src/app/services/autocompletion/autocompletion.service.ts
+++ front-end/src/app/services/autocompletion/autocompletion.service.ts
@@ -1,36 +1,21 @@
-import { Injectable, Injector } from '@angular/core'
+import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 
 @Injectable({
     providedIn: 'root'
 })
 export class AutocompletionService {
-    private http!: HttpClient
     private readonly countriesUrl =
         'https://countriesnow.space/api/v0.1/countries'
     private readonly currenciesUrl = 'https://restcountries.com/v3.1/all'
 
-    constructor(private readonly injector: Injector) {}
-
-    private getHttp(): HttpClient {
-        if (!this.http) {
-            this.http = this.injector.get(HttpClient)
-        }
-        return this.http
-    }
+    constructor(private readonly http: HttpClient) {}
 
     getCountries() {
-        console.log('getCountries dada t bien ri')
-        return this.getHttp().get(`${this.countriesUrl}/info`)
-    }
-
-    getCities(country: string) {
-        return this.getHttp().post(`${this.countriesUrl}/cities`, { country })
+        return this.http.get(`${this.countriesUrl}`)
     }
 
     getCurrencies() {
-        console.log('getCountries dada t bien ri ddd')
-
-        return this.getHttp().get(this.currenciesUrl)
+        return this.http.get(`${this.currenciesUrl}`)
     }
 }
