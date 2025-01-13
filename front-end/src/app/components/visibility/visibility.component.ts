e074fc8c399ea2c52805f827ae0ae67a236dfaa4
Renel Lherisson <renellherisson@gmail.com>
2025-01-13 11:32:16 -0500

    RL | user can see payment card clicking on the eye

---
+++ front-end/src/app/components/visibility/visibility.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-visibility',
+    imports: [],
+    templateUrl: './visibility.component.html',
+    styleUrl: './visibility.component.css'
+})
+export class VisibilityComponent {}
