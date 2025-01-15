3407b450ca90fbf670846fdfc0252467d5e12f8d
Renel Lherisson <renellherisson@gmail.com>
2025-01-14 20:56:14 -0500

    RL | change endpoints

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-3-93-171-162.compute-1.amazonaws.com:8000/payments'
+    serverUrl:
+        'http://ec2-3-93-171-162.compute-1.amazonaws.com:8000/api/payments'
 }
